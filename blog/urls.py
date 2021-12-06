eac4bcf6916323e1f3e2c23a058d1e3875071b34
Utugoed <petr.fanernyi@gmail.com>
2021-12-07 02:09:07 +0300

    Creating tags form was added.

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,12 @@
 
 from django.urls import path
-from .views import posts_list
+from .views import *
 
 
 urlpatterns = [
-    path('', posts_list)
+    path('', posts_list, name='posts_list_url'),
+    path('/posts/<str:slug>/', PostDetail.as_view(), name='post_detail_url'),
+    path('tags/', tags_list, name='tags_list_url'),
+    path('tag/create',TagCreate.as_view(), name='tag_create_url'),
+    path('tag/<str:slug>/', TagDetail.as_view(), name='tag_detail_url')
 ]
